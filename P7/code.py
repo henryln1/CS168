b13ce50c28faf83b116d80bdcda0666d33166209
Kaylee Bement <kbement@stanford.edu>
2018-05-21 22:08:29 -0700

    working on 1

--- P7/code.py
+++ P7/code.py
@@ -1,19 +1,73 @@
 import random
 import pandas as pd 
 import numpy as np 
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import math
 
 # Question 1
 
-# PART A stuff
-# part_1_a_3 = np.matrix([[0, 0.33, 0, 0, 0.33, 0, 0, 0, 0.33], [0.5, 0, 0.5, 0, 0, 0, 0, 0, 0], [0, 0.5, 0, 0.5, 0, 0, 0, 0, 0], [0, 0, 0.5, 0, 0.5, 0, 0, 0, 0], [0.33, 0, 0, 0.33, 0, 0.33, 0, 0, 0], [0, 0, 0, 0, 0.5, 0, 0.5, 0, 0], [0, 0, 0, 0, 0, 0.5, 0, 0.5, 0], [0, 0, 0, 0, 0, 0, 0.5, 0, 0.5], [0.5, 0, 0, 0, 0, 0, 0, 0.5, 0]])
-# print(part_1_a_3)
-# print(np.linalg.eig(part_1_a_3.T))
-# part_1_a_2 = np.matrix([[0, 0.5, 0, 0, 0, 0, 0, 0, 0.5], [0.5, 0, 0.5, 0, 0, 0, 0, 0, 0], [0, 0.5, 0, 0.5, 0, 0, 0, 0, 0], [0, 0, 0.5, 0, 0.5, 0, 0, 0, 0], [0, 0, 0, 0.5, 0, 0.5, 0, 0, 0], [0, 0, 0, 0, 0.5, 0, 0.5, 0, 0], [0, 0, 0, 0, 0, 0.5, 0, 0.5, 0], [0, 0, 0, 0, 0, 0, 0.5, 0, 0.5], [0.5, 0, 0, 0, 0, 0, 0, 0.5, 0]])
-# print(part_1_a_2)
-# print(np.linalg.eig(part_1_a_2.T))
+# PART A 
+part_1_1 = np.matrix([[0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0.5], [0.5, 0, 0.5, 0, 0, 0, 0, 0, 0, 0], [0, 0.5, 0, 0.5, 0, 0, 0, 0, 0, 0], [0, 0, 0.5, 0, 0.5, 0, 0, 0, 0, 0], [0, 0, 0, 0.5, 0, 0.5, 0, 0, 0, 0], [0, 0, 0, 0, 0.5, 0, 0.5, 0, 0, 0], [0, 0, 0, 0, 0, 0.5, 0, 0.5, 0, 0], [0, 0, 0, 0, 0, 0, 0.5, 0, 0.5, 0], [0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0.5], [0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0.5]])
+print(part_1_1)
+print(part_1_1.shape)
+stationary_dist_1 = np.array([0.1 for i in range(10)])
+part_1_2 = np.matrix([[0, 0.5, 0, 0, 0, 0, 0, 0, 0.5], [0.5, 0, 0.5, 0, 0, 0, 0, 0, 0], [0, 0.5, 0, 0.5, 0, 0, 0, 0, 0], [0, 0, 0.5, 0, 0.5, 0, 0, 0, 0], [0, 0, 0, 0.5, 0, 0.5, 0, 0, 0], [0, 0, 0, 0, 0.5, 0, 0.5, 0, 0], [0, 0, 0, 0, 0, 0.5, 0, 0.5, 0], [0, 0, 0, 0, 0, 0, 0.5, 0, 0.5], [0.5, 0, 0, 0, 0, 0, 0, 0.5, 0]])
+print(part_1_2)
+dist_vec = np.linalg.eig(part_1_2.T)[1][:,3]
+stationary_dist_2_nonorm = np.squeeze(np.asarray(dist_vec))
+stationary_dist_2 = stationary_dist_2_nonorm / sum(stationary_dist_2_nonorm)
+print(stationary_dist_2)
+part_1_3 = np.matrix([[0, 1./3, 0, 0, 1./3, 0, 0, 0, 1./3], [0.5, 0, 0.5, 0, 0, 0, 0, 0, 0], [0, 0.5, 0, 0.5, 0, 0, 0, 0, 0], [0, 0, 0.5, 0, 0.5, 0, 0, 0, 0], [1./3, 0, 0, 1./3, 0, 1./3, 0, 0, 0], [0, 0, 0, 0, 0.5, 0, 0.5, 0, 0], [0, 0, 0, 0, 0, 0.5, 0, 0.5, 0], [0, 0, 0, 0, 0, 0, 0.5, 0, 0.5], [0.5, 0, 0, 0, 0, 0, 0, 0.5, 0]])
+print(part_1_3)
+dist_vec_2 = np.linalg.eig(part_1_3.T)[1][:,4]
+stationary_dist_3_nonorm = np.squeeze(np.asarray(dist_vec_2))
+stationary_dist_3 = stationary_dist_3_nonorm / sum(stationary_dist_3_nonorm)
+print(stationary_dist_3)
 
+# PART B
+state_s_1 = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+state_s_2 = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0])
+state_s_3 = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0])
+times = [i for i in range(101)]
+markov_dists_1 = []
+markov_dists_2 = []
+markov_dists_3 = []
+
+for time in times:
+    dist_1 = 0
+    dist_2 = 0
+    dist_3 = 0
+
+    for i in range(len(state_s_1)):
+        dist_1 += abs(state_s_1[i] - stationary_dist_1[i])
+    for i in range(len(state_s_3)):
+        dist_2 += abs(state_s_2[i] - stationary_dist_2[i])
+        dist_3 += abs(state_s_3[i] - stationary_dist_3[i])
+    dist_1 /= 2
+    dist_2 /= 2
+    dist_3 /= 2
+
+
+    markov_dists_1.append(dist_1)
+    markov_dists_2.append(dist_2)
+    markov_dists_3.append(dist_3)
+
+    state_s_1 = np.squeeze(np.asarray(np.dot(state_s_1, part_1_1)))
+    state_s_2 = np.squeeze(np.asarray(np.dot(state_s_2, part_1_2)))
+    state_s_3 = np.squeeze(np.asarray(np.dot(state_s_3, part_1_3)))
+
+print(state_s_2)
+plt.plot(times, markov_dists_1, 'rs', label="Markov Chain 1")
+plt.plot(times, markov_dists_2, 'bs', label="Markov Chain 2")
+plt.plot(times, markov_dists_3, 'gs', label="Markov Chain 3")
+plt.title("Total Variation Distance over Time")
+plt.xlabel("t")
+plt.ylabel("total variation distance")
+plt.legend(shadow=True, loc=0)
+plt.savefig("1_b.png", format = 'png')
+plt.close()
 
 
 #Question 2
@@ -139,10 +193,10 @@ import math
 
 
 
-from QWOP import sim
+# from QWOP import sim
 
-#Question 3
+# #Question 3
 
-plan =[random.uniform(-1, 1) for x in range(40)]
+# plan =[random.uniform(-1, 1) for x in range(40)]
 
-sim(plan)
+# sim(plan)
